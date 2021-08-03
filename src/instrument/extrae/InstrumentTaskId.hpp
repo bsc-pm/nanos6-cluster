cbc2b6c7fb6f0ec2a58654c27aae04e92d8956b6
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2021-08-03 04:45:41 +0200

    Simplify instrumentation taskinfo.
    * src/instrument/extrae/InstrumentTaskId.hpp (TaskInfo) : Remove forward
    declaration and repeated code.

--- src/instrument/extrae/InstrumentTaskId.hpp
+++ src/instrument/extrae/InstrumentTaskId.hpp
@@ -22,25 +22,48 @@ namespace Instrument {
 	namespace Extrae {
 		typedef std::pair<size_t, dependency_tag_t> predecessor_entry_t; // Task and strength
 
-		struct TaskInfo;
-	}
+		struct TaskInfo {
+			nanos6_task_info_t *_taskInfo;
+			size_t _taskId;
+			int _nestingLevel;
+			long _priority;
+			Instrument::Extrae::TaskInfo *_parent;
 
+			std::atomic<bool> _inTaskwait;
+
+			SpinLock _lock;
+			std::set<predecessor_entry_t> _predecessors;
+
+			TaskInfo()
+				: _taskInfo(nullptr), _taskId(~0UL), _nestingLevel(-1), _priority(0), _parent(),
+				  _inTaskwait(false), _lock(), _predecessors()
+			{
+			}
+
+			TaskInfo(
+				nanos6_task_info_t *taskInfo,
+				int nestingLevel,
+				Instrument::Extrae::TaskInfo *parent
+			) : _taskInfo(taskInfo), _nestingLevel(nestingLevel), _priority(0), _parent(parent),
+				_inTaskwait(false), _lock(), _predecessors()
+			{
+				_taskId = _nextTaskId++;
+			}
+		};
+	}
 
 	struct task_id_t {
 		Extrae::TaskInfo *_taskInfo;
 
-		task_id_t()
-			: _taskInfo(nullptr)
+		task_id_t() : _taskInfo(nullptr)
 		{
 		}
 
-		task_id_t(Extrae::TaskInfo *taskInfo)
-			: _taskInfo(taskInfo)
+		task_id_t(Extrae::TaskInfo *taskInfo) : _taskInfo(taskInfo)
 		{
 		}
 
-		task_id_t(task_id_t const &other)
-			: _taskInfo(other._taskInfo)
+		task_id_t(task_id_t const &other) : _taskInfo(other._taskInfo)
 		{
 		}
 
@@ -61,47 +84,14 @@ namespace Instrument {
 			return (_taskInfo < other._taskInfo);
 		}
 
-		inline operator long() const;
-	};
-
-
-	namespace Extrae {
-		struct TaskInfo {
-			nanos6_task_info_t *_taskInfo;
-			size_t _taskId;
-			int _nestingLevel;
-			long _priority;
-			Instrument::Extrae::TaskInfo *_parent;
-
-			std::atomic<bool> _inTaskwait;
-
-			SpinLock _lock;
-			std::set<predecessor_entry_t> _predecessors;
-
-			TaskInfo()
-				: _taskInfo(nullptr), _taskId(~0UL), _nestingLevel(-1), _priority(0), _parent(),
-				_inTaskwait(false), _lock(), _predecessors()
-			{
+		operator long() const
+		{
+			if (_taskInfo != nullptr) {
+				return (long) _taskInfo->_taskId;
 			}
-
-			TaskInfo(nanos6_task_info_t *taskInfo, int nestingLevel, Instrument::Extrae::TaskInfo *parent)
-				: _taskInfo(taskInfo), _nestingLevel(nestingLevel), _priority(0), _parent(parent),
-				_inTaskwait(false), _lock(), _predecessors()
-			{
-				_taskId = _nextTaskId++;
-			}
-		};
-	}
-
-
-	inline task_id_t::operator long() const
-	{
-		if (_taskInfo != nullptr) {
-			return (long) _taskInfo->_taskId;
-		} else {
 			return 0;
 		}
-	}
+	};
 }
 
 
