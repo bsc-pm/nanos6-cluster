bab1de8e096ae17d841b58f03a90bc694f5fff94
Paul Carpenter <paul.carpenter@bsc.es>
2021-11-11 21:31:34 +0100

    Use LinearRegionMap in WriteID
    As preparation for dropping disableReadPropagationToNext, the WriteID needs to
    be improved to work much better when regions get fragmented.  Instead of
    storing a single region and its WriteID, we now store the regions (for a given
    hashed WriteID) in a LinearRegionMap. This means that subaccesses are still
    identified as present, as are larger accesses, all of whose parts are present.
    We hash the WriteID before looking up in a tree in order to reduce the overhead
    of locking, since different WriteIDs can be updated and accessed concurrently.

--- src/cluster/WriteID.hpp
+++ src/cluster/WriteID.hpp
@@ -14,33 +14,65 @@
 #include <vector>
 #include <iostream>
 
+#include "lowlevel/RWSpinLock.hpp"
 #include "DataAccessRegion.hpp"
+#include "LinearRegionMap.hpp"
+#include "LinearRegionMapImplementation.hpp"
 
-// Identify redundant data fetches using a write ID, which is shared among the
-// cluster nodes. Each new data output or inout is allocated a new write ID.
-// The write ID is propagated to the next dependent read only if the read is
-// for exactly the same region, otherwise the region has an invalid write ID of
-// zero. These functions check that both the write ID and the region match.
+// Identify redundant data fetches using a globally unique write ID.  Each new
+// data output or inout is allocated a new write ID.  The write ID remains the
+// same for all dependent read accesses.
 //
-// False negatives (claiming no local copy of a write ID when there is one) are
-// safe. This causes an unnecessary (but correct) data fetch.
+// We hold the tree of locally present regions and WriteIDs in a
+// LinearRegionMap, which means that subaccesses of the original access are
+// also identified as locally present.  Larger accesses, all of whose parts
+// have been registered as present, are also identified as such. The API does
+// not however, let us identify which parts (if not all) of a larger access are
+// present.
 //
-// False positives (claiming there is a local copy when there isn't one) will
-// cause incorrect execution so need to be avoided. This can only happen if two
-// different writes to the same region create the same write ID AND the old
-// write ID is present on a node that accesses this data. Since write IDs are
-// 64-bit and a cache contains for example 256 recent write IDs, there is a
-// probability of 1 in 2^48 that any call to 'checkWriteIDLocal' will return a
-// false positive.  Assuming that every task accesses this region, this allows
-// 2^41 tasks to be executed before there is (almost) a one-in-100 chance of a
-// false positive.
-
+// Accessing this tree requires taking a (reader/writer) lock, so we hash the
+// WriteID and use one of a small number of trees. The number of trees is given
+// by numMaps. This means that read and updates for the trees corresponding to
+// different WriteIDs can be done concurrently. Note: Different values of
+// numMaps does not change the behaviour of WriteID, but it may affect the
+// locking overhead.
+//
+// Note: the old WriteID is not removed when the same region with a later
+// WriteID is registered as present. But the dependency system already ensures
+// that we only check the WriteID for the currently-valid WriteID.
 
 // 64-bit Write ID.
 typedef size_t WriteID;
 
 typedef size_t HashID;
 typedef size_t WriteID;
+
+// Represent a region that is present locally with a given WriteID
+struct WriteIDEntryRegion {
+	DataAccessRegion _region;
+	WriteID _writeID;
+
+	DataAccessRegion const &getAccessRegion() const
+	{
+		return _region;
+	}
+
+	DataAccessRegion &getAccessRegion()
+	{
+		return _region;
+	}
+
+	WriteIDEntryRegion(DataAccessRegion region) : _region(region)
+	{
+	}
+};
+
+typedef LinearRegionMap<WriteIDEntryRegion> WriteIDLinearRegionMap;
+
+struct WriteIDEntry {
+	RWSpinLock _lock;
+	WriteIDLinearRegionMap _regions;
+};
 
 class WriteIDManager
 {
@@ -50,20 +82,14 @@ private:
 
 	/* Counter */
 	std::atomic<WriteID> _counter;
-	struct keypair {
-		HashID key;
-		DataAccessRegion value;
-	};
 
-	static constexpr int cacheSize = 8192;
-	std::vector<keypair> _localWriteIDs;
+	static constexpr int numMaps = 512;
+	std::vector<WriteIDEntry> _localWriteIDs;
 
-	static HashID hash(WriteID id, const DataAccessRegion &region)
+	static HashID hash(WriteID id)
 	{
-		size_t addr = (size_t)region.getStartAddress();
-		size_t size = (size_t)region.getSize();
 		// Based on https://xorshift.di.unimi.it/splitmix64.c
-		uint64_t z =  id + addr * 1234567 + size * 7654321;
+		uint64_t z =  id;
 		z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
 		z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
 		return z ^ (z >> 31);
@@ -71,7 +97,7 @@ private:
 
 public:
 
-	WriteIDManager(WriteID initCounter) : _localWriteIDs(cacheSize)
+	WriteIDManager(WriteID initCounter) : _localWriteIDs(numMaps)
 	{
 		_counter.store(initCounter);
 	}
@@ -86,7 +112,6 @@ public:
 		WriteID initCounter = 1 + (((size_t)nodeIndex) << (64 - logMaxNodes));
 
 		_singleton = new WriteIDManager(initCounter);
-		//std::cout << "construct WriteIDManager " << nodeIndex << " with counter: " << initCounter << "\n";
 	}
 
 	static void finalize()
@@ -101,33 +126,64 @@ public:
 	{
 		if (id) {
 			assert(_singleton != nullptr);
-			HashID key = hash(id, region);
-			_singleton->_localWriteIDs.at(key % cacheSize) = {key, region};
+
+			// Find the right tree and take a writer lock
+			int idx = hash(id) % numMaps;
+			WriteIDEntry &entry = _singleton->_localWriteIDs.at(idx);
+			entry._lock.writeLock();
+
+			// Update the tree
+			entry._regions.processIntersectingAndMissing(region,
+				[&](__attribute__((unused)) WriteIDLinearRegionMap::iterator position) -> bool {
+					// Region already in the map: update the writeID if it has changed
+					if (position->_writeID != id) {
+						if (!position->getAccessRegion().fullyContainedIn(region)) {
+							position = entry._regions.fragmentByIntersection(position, region, /* removeIntersection */ false);
+						}
+						position->_writeID = id;
+					}
+					return true;
+				},
+				[&](DataAccessRegion const &missingRegion) -> bool {
+					// Region not yet in the map: insert it with the given WriteID
+					WriteIDLinearRegionMap::iterator position = entry._regions.emplace(missingRegion);
+					position->_writeID = id;
+					return true;
+				}
+			);
+
+			entry._lock.writeUnlock();
 		}
 	}
 
-	// Register a write ID as not being present locally
-	// (this will require reclaiming memory which is not
-	// implemented yet).
-	static void unregisterWriteIDnotLocal(WriteID id, const DataAccessRegion &region)
-	{
-		assert(_singleton != nullptr);
-		HashID key = hash(id, region);
-		_singleton->_localWriteIDs.at(key % cacheSize).key = 0;
-	}
-
-	// Check whether a write ID is definitely present locally.
+	// Check whether the whole region for a write ID is definitely present locally.
 	static bool checkWriteIDLocal(WriteID id, const DataAccessRegion &region)
 	{
 		if (id) {
 			assert(_singleton != nullptr);
-			HashID key = hash(id, region);
-			if (key) {
-				const keypair &pair = _singleton->_localWriteIDs.at(key % cacheSize);
-				if (pair.key == key && pair.value == region) {
+
+			// Find the right tree and take a reader lock
+			int idx = hash(id) % numMaps;
+			WriteIDEntry &entry = _singleton->_localWriteIDs.at(idx);
+			size_t bytesFound = 0;
+			entry._lock.readLock();
+
+			// Add up all the bytes in this region that correspond to the correct WriteID
+			entry._regions.processIntersecting(region,
+				[&](WriteIDLinearRegionMap::iterator position) -> bool {
+					if (position->_writeID == id) {
+						const DataAccessRegion foundRegion = position->getAccessRegion();
+						DataAccessRegion subregion = foundRegion.intersect(region);
+						bytesFound += subregion.getSize();
+					}
 					return true;
 				}
-			}
+			);
+			entry._lock.readUnlock();
+
+			// Return true if all of the bytes of the region have been found
+			assert(bytesFound <= region.getSize());
+			return bytesFound == region.getSize();
 		}
 		return false;
 	}
